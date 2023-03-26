612dd62720fcef3d1907221cc27f8173502d02f2
Reagan <reagan.kelley@wsu.edu>
2023-03-25 19:03:50 -0700

    Added logic for do moves from PGN.

--- Chess/src/BoardState.cs
+++ Chess/src/BoardState.cs
@@ -45,11 +45,33 @@ namespace Chess
 
         public void Move(string moveNotation)
         {
+            if(moveNotation.Equals("O-O"))
+            {
+                // castle kings side
+                if (turn == 0)
+                {
+                    // white move
+                    this.whiteKingMoved = true;
+                    this.Castle(true, true);
+                    return;
+                    
+                }
+                else
+                {
+                    // black move
+                    this.blackKingMoved = true;
+                    this.Castle(false, true);
+                    return;
+                }
+            }
+
             string square2 = moveNotation;
+           
             if (moveNotation.Count() == 2)
             {
                 // this is a pawn move
                 int board_index = GetIndexOfPositionArray(square2);
+
 
                 if(turn == 0)
                 {
@@ -66,7 +88,6 @@ namespace Chess
                 else
                 {
                     // black's turn
-                    // white's turn
                     for (int i = 1; i <= 2; i++)
                     {
                         if (this.configuration[board_index - (i * 8)].Type == PieceType.Black_Pawn)
@@ -75,6 +96,102 @@ namespace Chess
                             this.Move(square1, square2);
                         }
                     }
+                }
+            }
+            else
+            {
+                string[] parts = moveNotation.Split('x');
+
+                if (parts.Count() == 2)
+                {
+                    if ((int)(parts[0][0]) >= 'a' && (int)(parts[0][0]) <= 'h')
+                    {
+                        int board_index = GetIndexOfPositionArray(parts[1]);
+                        
+                        // Pawn take move
+                        if (turn == 0)
+                        {
+                            // white's turn
+                            string square1 = GetSquareFromIndex(board_index + (parts[0][0] - parts[1][0]) + 8);
+                            square2 = parts[1];
+                            this.Move(square1, square2);
+                        }
+                        else
+                        {
+                            // black's turn
+                            string square1 = GetSquareFromIndex(board_index + (parts[0][0] - parts[1][0]) - 8);
+                            square2 = parts[1];
+                            this.Move(square1, square2);
+                        }
+                    }
+                    else
+                    {
+                        // Determine what piece 
+                        int piece_index = -1;
+                        switch (parts[0][0])
+                        {
+                            case 'Q':
+                                if (turn == 0)
+                                {
+                                    piece_index = LocateIndexOfPiece(PieceType.White_Queen);
+                                }
+                                else
+                                {
+                                    piece_index = LocateIndexOfPiece(PieceType.Black_Queen);
+                                }
+                                break;
+                            case 'B':
+                                this.Move(parts[0] + parts[1]); // call like moving to that square and return
+                                return;
+                        }
+                        string square1 = GetSquareFromIndex(piece_index);
+                        square2 = parts[1];
+
+                       this.Move(square1, square2);
+                    }
+                }
+                else
+                {
+                    // Piece move (no take)
+
+                    int piece_index = -1;
+                    square2 = moveNotation.Substring(1);
+                    switch (moveNotation[0])
+                    {
+                        case 'Q':
+                            if (turn == 0)
+                            {
+                                piece_index = LocateIndexOfPiece(PieceType.White_Queen);
+                            }
+                            else
+                            {
+                                piece_index = LocateIndexOfPiece(PieceType.Black_Queen);
+                            }
+                            break;
+                        case 'N':
+                            if (turn == 0)
+                            {
+                                piece_index = LocateIndexOfPiece(PieceType.White_Knight, can_see : GetIndexOfPositionArray(square2));
+                            }
+                            else
+                            {
+                                piece_index = LocateIndexOfPiece(PieceType.Black_Knight, can_see: GetIndexOfPositionArray(square2));
+                            }
+                            break;
+                        case 'B':
+                            if (turn == 0)
+                            {
+                                piece_index = LocateIndexOfPiece(PieceType.White_Bishop, can_see: GetIndexOfPositionArray(square2));
+                            }
+                            else
+                            {
+                                piece_index = LocateIndexOfPiece(PieceType.Black_Bishop, can_see: GetIndexOfPositionArray(square2));
+                            }
+                            break;
+                    }
+
+                    string square1 = GetSquareFromIndex(piece_index);
+                    this.Move(square1, square2);
                 }
             }
         }
@@ -95,6 +212,168 @@ namespace Chess
             else
             {
                 this.turn = 1;
+            }
+        }
+
+        public void Castle(bool WhiteKing, bool right)
+        {
+            if (WhiteKing)
+            {
+                if (right)
+                {
+                    this.configuration[62].SetPiece(PieceType.White_King);
+                    this.configuration[60].SetPiece(PieceType.None);
+
+                    this.configuration[61].SetPiece(PieceType.White_Rook);
+                    this.configuration[63].SetPiece(PieceType.None);
+                }
+                else
+                {
+                    this.configuration[58].SetPiece(PieceType.White_King);
+                    this.configuration[60].SetPiece(PieceType.None);
+
+                    this.configuration[59].SetPiece(PieceType.White_Rook);
+                    this.configuration[56].SetPiece(PieceType.None);
+                }
+            }
+            else
+            {
+                if (right)
+                {
+                    this.configuration[6].SetPiece(PieceType.Black_King);
+                    this.configuration[4].SetPiece(PieceType.None);
+
+                    this.configuration[5].SetPiece(PieceType.Black_Rook);
+                    this.configuration[7].SetPiece(PieceType.None);
+                }
+                else
+                {
+                    this.configuration[6].SetPiece(PieceType.Black_King);
+                    this.configuration[1].SetPiece(PieceType.None);
+
+                    this.configuration[3].SetPiece(PieceType.Black_Rook);
+                    this.configuration[0].SetPiece(PieceType.None);
+                }
+            }
+
+            if (this.turn == 1)
+            {
+                this.moveNumber++;
+                this.turn = 0;
+            }
+            else
+            {
+                this.turn = 1;
+            }
+        }
+
+        private int LocateIndexOfPiece(PieceType target, int can_see = -1)
+        {
+            for (int i = 0; i < 64; i++)
+            {
+                if (this.configuration[i].Type == target)
+                {
+                    if (can_see < 0)
+                    {
+                        return i;
+                    }
+
+                    if (target == PieceType.Black_Knight || target == PieceType.White_Knight)
+                    {
+                        // Down 2, Right 1
+                        if (((i + 17) < 64) && (((i + 17) % 8) > 0))
+                        {
+                            if ((i + 17) == can_see)
+                            {
+                                return i;
+                            }
+                        }
+
+                        // Down 2, Left 1
+                        if (((i + 15) < 64) && ((i + 15) % 8) < 7)
+                        {
+                            if ((i + 15) == can_see)
+                            {
+                                return i;
+                            }
+                        }
+
+                        // Down 1, Right 2
+                        if (((i + 10) < 64) && (((i + 10) % 8) > 1))
+                        {
+                            if ((i + 10) == can_see)
+                            {
+                                return i;
+                            }
+                        }
+
+                        // Down 1, Left 2
+                        if (((i + 6) < 64) && ((i + 6) % 8) < 6)
+                        {
+                            if ((i + 6) == can_see)
+                            {
+                                return i;
+                            }
+                        }
+
+                        // Up 2, Right 1
+                        if (((i - 15) >= 0) && (((i - 15) % 8) > 0))
+                        {
+                            if ((i - 15) == can_see)
+                            {
+                                return i;
+                            }
+                        }
+
+                        // Up 2, Left 1
+                        if (((i - 17) >= 0) && ((i - 17) % 8) < 7)
+                        {
+                            if ((i - 17) == can_see)
+                            {
+                                return i;
+                            }
+                        }
+
+                        // Up 1, Right 2
+                        if (((i - 6) >= 0) && (((i - 6) % 8) > 1))
+                        {
+                            if ((i - 6) == can_see)
+                            {
+                                return i;
+                            }
+                        }
+
+                        // Up 1, Left 2
+                        if (((i - 10) >= 0) && ((i - 10) % 8) < 6)
+                        {
+                            if ((i - 10) == can_see)
+                            {
+                                return i;
+                            }
+                        }
+                    }
+                    else if (target == PieceType.White_Bishop || target == PieceType.Black_Bishop)
+                    {
+                        if (this.IsWhiteSquare(i) == this.IsWhiteSquare(can_see))
+                        {
+                            return i;
+                        }
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        private bool IsWhiteSquare(int index)
+        {
+            if ((index / 8) % 2 == 0)
+            {
+                return (index % 2 == 0);
+            }
+            else
+            {
+                return (index % 2 == 1);
             }
         }
 
